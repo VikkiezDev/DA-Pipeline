5b09d79934d81ede76b50d5c99266c29c8373c9e
Vikky Nadar <vigneshsn23@outlook.com>
2024-08-25 15:37:51 +0530

    Loaded all the files on Github

---
+++ src/etl/extract.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+url = "https://linkedin-data-api.p.rapidapi.com/search-jobs"
+
+querystring = {
+    "keywords": "python",
+    "locationId": "102956297",
+    "datePosted": "anyTime",
+    "sort": "mostRelevant"
+}
+
+headers = {
+    "x-rapidapi-key": "cafc3459fcmsh57b25e3bf42316ep19a521jsn572d50692ea8",
+    "x-rapidapi-host": "linkedin-data-api.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+# Assuming the response is JSON, store it in a variable
+data = response.json()
+
+# Specify the file path where you want to save the JSON data
+file_path = "/home/vignesh-nadar/Desktop/sixtyDays/sprint1/project1/data/temp.json"
+
+# Open the file in write mode and save the JSON data
+with open(file_path, 'w') as file:
+    json.dump(data, file, indent=4)
+
+print(f"Data saved to {file_path}")
+
